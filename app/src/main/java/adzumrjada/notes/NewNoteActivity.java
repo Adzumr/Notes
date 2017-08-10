f52e71409980094803b9f3066da6757915cb4696
Abdulaziz Umar Jada <adzumrjada@gmail.com>
2017-08-10 02:14:58 +0100

    Initial Cmmit

---
+++ app/src/main/java/adzumrjada/notes/NewNoteActivity.java
@@ -0,0 +1,23 @@
+package adzumrjada.notes;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class NewNoteActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_note);
+    }
+    public void Back(View view) {
+        Intent i = new Intent(this, MyNotesActivity.class);
+        startActivity(i);
+        }
+
+    }
+
+
+
+
